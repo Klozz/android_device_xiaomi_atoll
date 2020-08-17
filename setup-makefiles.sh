1ce3b048006fffc1251e864c3fb19cb106acc713
klozz <xXx.Reptar.Rawrr.xXx@gmail.com>
2020-08-17 18:01:28 -0500

    curtana: convert to non common setup-makefiles.sh
    Signed-off-by: klozz <xXx.Reptar.Rawrr.xXx@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -7,44 +7,33 @@
 
 set -e
 
+export DEVICE=curtana
+export VENDOR=xiaomi
 INITIAL_COPYRIGHT_YEAR=2020
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-AOSIP_ROOT="${MY_DIR}/../../.."
+XPERIENCE_ROOT="${MY_DIR}/../../.."
 
-HELPER="${AOSIP_ROOT}/vendor/aosip/build/tools/extract_utils.sh"
+HELPER="${XPERIENCE_ROOT}/vendor/xperience/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 source "${HELPER}"
 
-# Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${AOSIP_ROOT}" true
+# Reinitialize the helper for device
+INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
+setup_vendor "${DEVICE}" "${VENDOR}" "${XPERIENCE_ROOT}" false
 
 # Copyright headers and guards
-write_headers "curtana"
+    write_headers
 
-# The standard common blobs
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
+# The standard device blobs
+write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
 
 # Finish
 write_footers
 
-if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
-    # Reinitialize the helper for device
-    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${AOSIP_ROOT}" false
-
-    # Copyright headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
-
-    # Finish
-    write_footers
-fi
