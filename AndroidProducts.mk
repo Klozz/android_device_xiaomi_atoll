c1449591fcecdefa4e55a55f3341017ad3575c07
klozz <klozz@vivaldi.net>
2020-08-20 03:31:05 +0000

    atoll: import NFC , thermal and rootdir
    Signed-off-by: klozz <klozz@vivaldi.net>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/du.mk
+
+COMMON_LUNCH_CHOICES := \
+    du_miatoll-user \
+    du_miatoll-userdebug \
+    du_miatoll-eng
