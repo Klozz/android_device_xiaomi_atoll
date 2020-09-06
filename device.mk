b58afd8697c5045abf147570553dfa8099a9956a
klozz <klozz@vivaldi.net>
2020-09-06 03:56:59 +0000

    atoll:du: exclude vendor overlays if the rom builded is DU
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -188,6 +188,12 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap \
     $(LOCAL_PATH)/overlay-system
 
+ifeq ($(PRODUCT_ORS_DU)),true)
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/themes/overlay
+
+endif
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
