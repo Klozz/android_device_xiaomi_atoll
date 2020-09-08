0762cb22bdd2c7d2240f949efbcada078460ef53
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2020-09-08 02:44:53 -0500

    Atoll: Remove sm6250--common
    we have uncommonized vendor

--- device.mk
+++ device.mk
@@ -9,7 +9,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit proprietary targets
 $(call inherit-product-if-exists, vendor/xiaomi/miatoll/miatoll-vendor.mk)
-$(call inherit-product-if-exists, vendor/xiaomi/sm6250-common/sm6250-common-vendor.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
