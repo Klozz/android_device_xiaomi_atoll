Klozz/android_device_xiaomi_atoll

commit 6a4beb0d3e4b883875284a78670bce345a8a2702

Atoll: Add more Bluetooth packages
Signed-off-by: klozz <klozz@vivaldi.net>

device.mk
@@ -54,8 +54,25 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     Bluetooth \
+    BluetoothExt \
     libbluetooth_qti \
     libbt-vendor
+
+PRODUCT_PACKAGES += \	
+    libbt-hidlclient \
+    javax.btobex \
+    libattrib_static \
+    libbt-vendor \
+    libbt-logClient \
+    bt_logger \
+    libbluetooth_qti \
+    libbt-hidlclient \
+    libbtconfigstore \
+    com.qualcomm.qti.bluetooth_audio@1.0 \
+    vendor.qti.hardware.bluetooth_audio@2.0 \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0 \
+    vendor.qti.hardware.bluetooth_dun@1.0
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \

BoardConfig.mk
@@ -38,13 +38,16 @@ USE_CUSTOM_AUDIO_POLICY := 1
 USE_XML_AUDIO_POLICY_CONF := 1
 
 # Bluetooth
-# Bluetooth
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BOARD_HAS_QCA_BT_SOC := "cherokee"
 BLUETOOTH_HCI_USE_MCT := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 QCOM_BT_USE_BTNV := true
 QCOM_BT_USE_SMD_TTY := true
+TARGET_USE_QTI_BT_STACK := true
+TARGET_USE_QTI_BT_CONFIGSTORE := true
+TARGET_USE_QTI_BT_SAR := true
+TARGET_USE_QTI_VND_FWK_DETECT := true
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := miatoll

commit 68eeea820d4f1157bb1d289eb49d1a514227f5ea

miatoll: Fix typo introduced in 6a4beb0d3e4b883875284a78670bce345a8a270
Signed-off-by: klozz <klozz@vivaldi.net>

device.mk
@@ -58,7 +58,7 @@ PRODUCT_PACKAGES += \
     libbluetooth_qti \
     libbt-vendor
 
-PRODUCT_PACKAGES += \	
+PRODUCT_PACKAGES += \
     libbt-hidlclient \
     javax.btobex \
     libattrib_static \