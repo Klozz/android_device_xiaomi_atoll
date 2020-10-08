d94f2dbbb625cd8af252584f39720e2cf498ceea
klozz <klozz@vivaldi.net>
2020-10-08 05:14:01 +0000

    Atoll: track radio config 1.1
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -276,7 +276,7 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
     android.hardware.radio@1.2 \
-    android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.1 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libxml2 \
